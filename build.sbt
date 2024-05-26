d2b800cb81105ed9abcaf39cb6ff6da26023848d
yaroslav <Yaroslav>
2024-05-26 03:44:47 +0300

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,26 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.14"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "repository-metrics-scraper",
+    libraryDependencies ++= Seq(
+      "com.lihaoyi" %% "requests" % "0.8.0",
+      "com.softwaremill.sttp.client4" %% "core" % "4.0.0-M14",
+      "com.lihaoyi" %% "ujson" % "3.3.1",
+      "io.circe" %% "circe-core" % "0.14.7",
+      "io.circe" %% "circe-generic" % "0.14.7",
+      "io.circe" %% "circe-parser" % "0.14.7",
+
+      "org.typelevel" %% "cats-core" % "2.10.0",
+
+      // Start with this one
+      "org.tpolecat" %% "doobie-core"      % "1.0.0-RC4",
+
+      // And add any of these as needed
+      "org.tpolecat" %% "doobie-h2"        % "1.0.0-RC4",          // H2 driver 1.4.200 + type mappings.
+      "org.tpolecat" %% "doobie-hikari"    % "1.0.0-RC4",          // HikariCP transactor.
+      "org.tpolecat" %% "doobie-postgres"  % "1.0.0-RC4",          // Postgres driver 42.6.0 + type mappings.
+    )
+  )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,70 @@
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import doobie._
+import doobie.implicits._
+import io.circe._
+import io.circe.jawn.decode
+import requests.Response
+import sttp.client4.quick._
+import sttp.model.Uri
+
+case class GithubApiResponse(repositories: List[GithubRepository])
+
+object GithubApiResponse {
+  implicit val decoder: Decoder[GithubApiResponse] = (hCursor: HCursor) => {
+    for {
+      repos <- hCursor.get[List[GithubRepository]]("items")
+    } yield GithubApiResponse(repos)
+  }
+}
+
+case class GithubRepository(name: String, url: String, starsCount: Int)
+
+object GithubRepository {
+  implicit val decoder: Decoder[GithubRepository] = (hCursor: HCursor) => {
+    for {
+      name <- hCursor.get[String]("name")
+      url <- hCursor.get[String]("html_url")
+      starsCount <- hCursor.get[Int]("stargazers_count")
+    } yield GithubRepository(name, url, starsCount)
+  }
+}
+
+object Persistence {
+  def insertMultipleRepositories(repos: List[GithubRepository]): ConnectionIO[Int] = {
+    Update[GithubRepository](
+      """insert into repository (name, url, stars)
+         values (?, ?, ?)
+         """)
+      .updateMany(repos)
+  }
+}
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val xa = Transactor.fromDriverManager[IO](
+      driver = "org.postgresql.Driver",
+      url = "jdbc:postgresql://master.f94318ce-b33a-4cdb-a291-6a10fd934f15.c.dbaas.selcloud.ru:5432/metrics",
+      user = "yaroslav",
+      password = "",
+      logHandler = None
+    )
+    println("Hello world!")
+    val prefix: Uri = uri"https://api.github.com/search/repositories"
+    val queryParams = Map(
+      "q" -> "language:java",
+      "sort" -> "stars",
+      "order" -> "desc",
+      "page" -> "1"
+    )
+    val r: Response = requests.get(prefix.withParams(queryParams).toString())
+    val jsonData = decode[GithubApiResponse](r.data.toString())
+    println(jsonData match {
+      case Right(response) =>
+        Persistence.insertMultipleRepositories(response.repositories)
+          .transact(xa)
+        .unsafeRunSync()
+    })
+  }
+}
